3014fc94e4fb2e6a1b87376cc35e510ca8c318e8
victorgargor <cogorderosvictorino@gmail.com>
2025-04-03 16:54:59 +0200

    03/04/2025 Cambios de apariencia añadidos criterios de búsqueda y ordenación, iconos etc.

---
+++ BlazorAppVictor/Models/PaginatedResult.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorAppVictor.Models
+{
+    public class PaginatedResult<T>
+    {
+        public List<T> Items { get; set; } = new List<T>();
+        public int TotalCount { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+        public int PageSize { get; set; } = 5;
+        public int CurrentPage { get; set; }
+
+        // Constructor
+        public PaginatedResult(List<T> items, int totalCount, int currentPage, int pageSize = 5)
+        {
+            Items = items;
+            TotalCount = totalCount;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+        }
+    }
+}
